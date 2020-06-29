3241f6230777798d763ff9480fffa21619dd4c20
Kyle <chiefmccain85@gmail.com>
2020-06-29 11:03:00 -0600

    Reversed response and error parameters in callback

--- src/tmdb-api/tmdb-api-interface.js
+++ src/tmdb-api/tmdb-api-interface.js
@@ -10,15 +10,15 @@ const axios = require('axios')
 // Function that sends a GET request to the TMDb API 
 let tmdbGETRequest = (requestURL, callback) => {
   axios.get(requestURL, callback)
-    .then(response => callback(response))
-    .catch(error => callback(undefined, error));
+    .then(response => callback(null, response))
+    .catch(error => callback(error));
 }
 
 // Function that sends a POST request to the TMDb API 
 let tmdbPOSTRequest = (requestURL, options, callback) => {
   axios.post(requestURL, options, callback)
-    .then(response => callback(response))
-    .catch(error => callback(undefined, error));
+    .then(response => callback(null, response))
+    .catch(error => callback(error));
 }
 
 module.exports = {
